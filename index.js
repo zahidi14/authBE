f32041087f6ac24a3485aa4c6601fe9526304abd
zahidi14 <d.zahidi1412@gmail.com>
2023-11-27 09:08:51 +0800

    vercel json added

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const filter = (req, file, cb) => {
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://localhost:5173",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     credentials: true,
   })
